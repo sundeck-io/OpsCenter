5be19b35c05184f653502845c288b060dc728bb8
Doron Rosenberg <doronr@gmail.com>
2023-07-05 17:12:47 -0700

    cleanup

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   env: {
-    "OPSCENTER_URL": "http://localhost:8501"
+    OPSCENTER_URL: "http://localhost:8501",
   },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    supportFile: false
   },
 });
