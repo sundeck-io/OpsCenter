16379651411256033c7121c44325a67d9fdf04b1
Josh Elser <josh@sundeck.io>
2023-07-14 17:15:38 -0400

    Remove cypress database after check (#56)
    * Also clean up the app from deploy.py after pull-request

---
+++ deploy/cypress_teardown.py
@@ -0,0 +1,36 @@
+import getopt
+import helpers
+import sys
+
+
+def teardown(profile: str):
+    conn = helpers.connect_to_snowflake(profile=profile)
+    conn.cursor().execute(f"DROP DATABASE IF EXISTS {conn.database};")
+
+
+def usage():
+    print("cypress_teardown.py -p <snowsql_profile_name>")
+
+
+def main(argv):
+    """
+    Parse command line arguments and call devdeploy.
+    """
+    profile = "local_opscenter"
+    opts, args = getopt.getopt(argv, "hp:", ["profile="])
+    for opt, arg in opts:
+        if opt == "-h":
+            usage()
+            sys.exit()
+        elif opt in ("-p", "--profile"):
+            profile = arg
+
+    if profile is None:
+        usage()
+        sys.exit(1)
+
+    teardown(profile)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ deploy/pr_teardown.py
@@ -0,0 +1,36 @@
+import getopt
+import helpers
+import sys
+
+
+def teardown(profile: str, application: str):
+    conn = helpers.connect_to_snowflake(profile=profile)
+    conn.cursor().execute(f"DROP APPLICATION IF EXISTS {application};")
+
+
+def usage():
+    print("pr_teardown.py -p <snowsql_profile_name>")
+
+
+def main(argv):
+    profile = "local_opscenter"
+    application = None
+    opts, args = getopt.getopt(argv, "hpa:", ["profile=", "application="])
+    for opt, arg in opts:
+        if opt == "-h":
+            usage()
+            sys.exit()
+        elif opt in ("-p", "--profile"):
+            profile = arg
+        elif opt in ("-a", "--application"):
+            application = arg
+
+    if profile is None or application is None:
+        usage()
+        sys.exit(1)
+
+    teardown(profile, application)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
