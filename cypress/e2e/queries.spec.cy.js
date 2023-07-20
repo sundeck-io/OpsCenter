5d46422fe2bc1d785e64c306e66018e155a8f3a5
vqmarkman <104043363+vqmarkman@users.noreply.github.com>
2023-07-20 10:41:07 -0700

    Additional sanity tests for Query tab. (#66)
    Closes #63.

--- cypress/e2e/queries.spec.cy.js
+++ cypress/e2e/queries.spec.cy.js
@@ -5,6 +5,51 @@ describe("Queries section", () => {
     setup();
   });
 
+
+it("Menu: Queries (Top Spenders)", () => {
+    cy.visit("/");
+
+    cy.get("span")
+      .contains("Queries")
+      .should("be.visible")
+      .click();
+
+    dropDownOpen("Select Report");
+    dropDownElementClick("Top Spenders");
+    checkNoErrorOnThePage();
+
+    // List display option
+    dropDownOpen("Pick View");
+    dropDownElementClick("List");
+    checkNoErrorOnThePage();
+
+    // Graph display option
+    dropDownOpen("Pick View");
+    dropDownElementClick("Graph");
+    checkNoErrorOnThePage();
+
+    // Iterate over filters
+
+    const stringList= ["365", "90", "30", "7"];
+
+    cy.get("span")
+      .contains("Top Spenders")
+      .should("be.visible");
+
+    // Click on Filters
+    cy.get('div[data-testid="stMarkdownContainer"]')
+      .contains("Filters")
+      .click();
+
+    for (const str of stringList) {
+      cy.get('button[kind="secondary"]')
+        .contains(str)
+        .click();
+      checkNoErrorOnThePage();
+    }
+
+  });
+
 it("Menu: Queries (dbt Summary)", () => {
     cy.visit("/");
 
@@ -20,5 +65,73 @@ it("Menu: Queries (dbt Summary)", () => {
     dropDownOpen("Pick View");
     dropDownElementClick("List");
     checkNoErrorOnThePage();
-    });
+
+    // Iterate over filters
+
+    const stringList= ["365", "90", "30", "7"];
+
+    cy.get("span")
+      .contains("dbt Summary")
+      .should("be.visible");
+
+    // Click on Filters
+    cy.get('div[data-testid="stMarkdownContainer"]')
+      .contains("Filters")
+      .click();
+
+    for (const str of stringList) {
+      cy.get('button[kind="secondary"]')
+        .contains(str)
+        .click();
+      checkNoErrorOnThePage();
+    }
+
+});
+
+it("Menu: Queries (Query Activity)", () => {
+    cy.visit("/");
+
+    cy.get("span")
+      .contains("Queries")
+      .should("be.visible")
+      .click();
+
+    // Loop over category dropdown list, check no error on the page
+    const categorylList= ["User", "Warehouse", "Role", "Query Type", "Execution Status"];
+    for (const category of categorylList) {
+
+      dropDownOpen("Color by Category or Grouping Label");
+      dropDownElementClick(category);
+      checkNoErrorOnThePage();
+
+      // Test with Graph and List display options
+      dropDownOpen("Pick View");
+      dropDownElementClick("List");
+      checkNoErrorOnThePage();
+
+      dropDownOpen("Pick View");
+      dropDownElementClick("Graph");
+      checkNoErrorOnThePage();
+    }
+
+    // Iterate over filters
+
+    const stringList= ["365", "90", "30", "7"];
+
+    cy.get("span")
+      .contains("Query Activity")
+      .should("be.visible");
+
+    // Click on Filters
+    cy.get('div[data-testid="stMarkdownContainer"]')
+      .contains("Filters")
+      .click();
+
+    for (const str of stringList) {
+      cy.get('button[kind="secondary"]')
+        .contains(str)
+        .click();
+      checkNoErrorOnThePage();
+    }
+ });
 });
