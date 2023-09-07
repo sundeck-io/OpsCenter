sundeck-io/OpsCenter

commit 0d88aae747c6f449274fa7143e566aaa5195782a

We should take column names from snowflake tables now that our migration code works (#243)

bootstrap/090_post_setup.sql
@@ -11,8 +11,8 @@ BEGIN
           c1 CURSOR FOR
               with columns as (
                 select table_schema, table_name, LISTAGG('"' || COLUMN_NAME || '" ', ', \n') WITHIN GROUP (ORDER BY ORDINAL_POSITION) as cols
-                from information_schema.columns
-                where table_catalog = current_database() and table_schema in ('ACCOUNT_USAGE', 'ORGANIZATION_USAGE')
+                from snowflake.information_schema.columns
+                where table_catalog = 'SNOWFLAKE' and table_schema in ('ACCOUNT_USAGE', 'ORGANIZATION_USAGE')
                 group by table_name, table_schema
               ), delays as (
             select $1 as table_name, $2 as delay, $3 as ts from (values ('QUERY_HISTORY', 45, 'END_TIME'), ('WAREHOUSE_EVENTS_HISTORY', 180, 'TIMESTAMP'), ('WAREHOUSE_LOAD_HISTORY', 180, 'END_TIME'), ('WAREHOUSE_METERING_HISTORY', 180, 'END_TIME'), ('USERS', 120, 'CREATED_ON'), ('SERVERLESS_TASK_HISTORY', 180, 'END_TIME'))

commit 532f685222b81b4d416f4db0af018af4e3e6e23b

Correctly use snowflake schema as base schema
this fixes the bug introduced in #243 and reverted in #256. The bug was
related to migrating views but not the underlying tables. In finalize
setup we make sure both tables and views are in sync once we get
permission to access snowflake account usage tables.

closes #254

bootstrap/018_warehouse_updates.sql
@@ -18,6 +18,22 @@ EXCEPTION
 END;
 call internal.migrate_warehouse_size_mapping();
 
+CREATE OR REPLACE PROCEDURE internal.migrate_warehouse_events()
+returns variant
+language sql
+as
+begin
+    SYSTEM$LOG_TRACE('Migrating warehouse events data.');
+    call internal.migrate_if_necessary('INTERNAL_REPORTING', 'CLUSTER_AND_WAREHOUSE_SESSIONS_COMPLETE_AND_DAILY', 'INTERNAL_REPORTING_MV', 'CLUSTER_AND_WAREHOUSE_SESSIONS_COMPLETE_AND_DAILY');
+    let migrate1 variant := (select * from TABLE(RESULT_SCAN(LAST_QUERY_ID())));
+    call internal.migrate_if_necessary('INTERNAL_REPORTING', 'CLUSTER_AND_WAREHOUSE_SESSIONS_COMPLETE_AND_DAILY', 'INTERNAL_REPORTING_MV', 'CLUSTER_AND_WAREHOUSE_SESSIONS_COMPLETE_AND_DAILY_INCOMPLETE');
+    let migrate2 variant := (select * from TABLE(RESULT_SCAN(LAST_QUERY_ID())));
+    -- Ensure that RECORD_TYPE is VARCHAR and not VARCHAR(8)
+    ALTER TABLE INTERNAL_REPORTING_MV.CLUSTER_AND_WAREHOUSE_SESSIONS_COMPLETE_AND_DAILY MODIFY COLUMN RECORD_TYPE TYPE VARCHAR;
+    ALTER TABLE INTERNAL_REPORTING_MV.CLUSTER_AND_WAREHOUSE_SESSIONS_COMPLETE_AND_DAILY_INCOMPLETE MODIFY COLUMN RECORD_TYPE TYPE VARCHAR;
+    return object_construct('migrate1', migrate1, 'migrate2', migrate2);
+end;
+
 CREATE OR REPLACE PROCEDURE internal.refresh_warehouse_events(migrate boolean) RETURNS STRING LANGUAGE SQL
     COMMENT = 'Refreshes the warehouse events materialized view. If migrate is true, then the materialized view will be migrated if necessary.'
     AS
@@ -27,14 +43,10 @@ BEGIN
     let migrate1 string := null;
     let migrate2 string := null;
     if (migrate) then
-        SYSTEM$LOG_TRACE('Migrating warehouse events data.');
-        call internal.migrate_if_necessary('INTERNAL_REPORTING', 'CLUSTER_AND_WAREHOUSE_SESSIONS_COMPLETE_AND_DAILY', 'INTERNAL_REPORTING_MV', 'CLUSTER_AND_WAREHOUSE_SESSIONS_COMPLETE_AND_DAILY');
-        migrate1 := (select * from TABLE(RESULT_SCAN(LAST_QUERY_ID())));
-        call internal.migrate_if_necessary('INTERNAL_REPORTING', 'CLUSTER_AND_WAREHOUSE_SESSIONS_COMPLETE_AND_DAILY', 'INTERNAL_REPORTING_MV', 'CLUSTER_AND_WAREHOUSE_SESSIONS_COMPLETE_AND_DAILY_INCOMPLETE');
-        migrate2 := (select * from TABLE(RESULT_SCAN(LAST_QUERY_ID())));
-        -- Ensure that RECORD_TYPE is VARCHAR and not VARCHAR(8)
-        ALTER TABLE INTERNAL_REPORTING_MV.CLUSTER_AND_WAREHOUSE_SESSIONS_COMPLETE_AND_DAILY MODIFY COLUMN RECORD_TYPE TYPE VARCHAR;
-        ALTER TABLE INTERNAL_REPORTING_MV.CLUSTER_AND_WAREHOUSE_SESSIONS_COMPLETE_AND_DAILY_INCOMPLETE MODIFY COLUMN RECORD_TYPE TYPE VARCHAR;
+        let migrate_result variant variant;
+        call internal.migrate_warehouse_events() into migrate_result;
+        migrate1 := migrate_result:migrate1::string;
+        migrate2 := migrate_result:migrate2::string;
     end if;
 
     let input variant := null;

bootstrap/019_query_history_updates.sql
@@ -1,6 +1,20 @@
 
 CREATE TABLE INTERNAL.TASK_QUERY_HISTORY IF NOT EXISTS (run timestamp, success boolean, input variant, output variant);
 
+CREATE OR REPLACE PROCEDURE internal.migrate_queries()
+returns variant
+language sql
+as
+begin
+    SYSTEM$LOG_TRACE('Migrating query history data.');
+    call internal.migrate_view();
+    call internal.migrate_if_necessary('INTERNAL_REPORTING', 'QUERY_HISTORY_COMPLETE_AND_DAILY', 'INTERNAL_REPORTING_MV', 'QUERY_HISTORY_COMPLETE_AND_DAILY');
+    let migrate1 string := (select * from TABLE(RESULT_SCAN(LAST_QUERY_ID())));
+    call internal.migrate_if_necessary('INTERNAL_REPORTING', 'QUERY_HISTORY_COMPLETE_AND_DAILY', 'INTERNAL_REPORTING_MV', 'QUERY_HISTORY_COMPLETE_AND_DAILY_INCOMPLETE');
+    let migrate2 string := (select * from TABLE(RESULT_SCAN(LAST_QUERY_ID())));
+    return object_construct('migrate1', migrate1, 'migrate2', migrate2);
+end;
+
 CREATE OR REPLACE PROCEDURE internal.refresh_queries(migrate boolean) RETURNS STRING LANGUAGE SQL AS
 BEGIN
     let dt timestamp := current_timestamp();
@@ -8,15 +22,10 @@ BEGIN
     let migrate1 string := null;
     let migrate2 string := null;
     if (migrate) then
-        SYSTEM$LOG_TRACE('Migrating query history data.');
-        call internal.migrate_view();
-        call internal.migrate_if_necessary('INTERNAL_REPORTING', 'QUERY_HISTORY_COMPLETE_AND_DAILY', 'INTERNAL_REPORTING_MV', 'QUERY_HISTORY_COMPLETE_AND_DAILY');
-        migrate1 := (select * from TABLE(RESULT_SCAN(LAST_QUERY_ID())));
-        call internal.migrate_if_necessary('INTERNAL_REPORTING', 'QUERY_HISTORY_COMPLETE_AND_DAILY', 'INTERNAL_REPORTING_MV', 'QUERY_HISTORY_COMPLETE_AND_DAILY_INCOMPLETE');
-        migrate2 := (select * from TABLE(RESULT_SCAN(LAST_QUERY_ID())));
-        -- Ensure that RECORD_TYPE is VARCHAR and not VARCHAR(8)
-        ALTER TABLE INTERNAL_REPORTING_MV.QUERY_HISTORY_COMPLETE_AND_DAILY MODIFY COLUMN RECORD_TYPE TYPE VARCHAR;
-        ALTER TABLE INTERNAL_REPORTING_MV.QUERY_HISTORY_COMPLETE_AND_DAILY_INCOMPLETE MODIFY COLUMN RECORD_TYPE TYPE VARCHAR;
+        let migration_result variant;
+        call internal.migrate_queries() into :migration_result;
+        migrate1 := migration_result:migrate1::string;
+        migrate2 := migration_result:migrate2::string;
     end if;
 
     let input variant := null;

bootstrap/090_post_setup.sql
@@ -32,6 +32,9 @@ BEGIN
             END FOR;
         END;
     END;
+call internal.migrate_queries();
+call internal.migrate_warehouse_events();
+call internal.migrate_view();
 
 -- These can't be created until after EXECUTE MANAGED TASK is granted to application.
 CREATE OR REPLACE TASK TASKS.WAREHOUSE_EVENTS_MAINTENANCE