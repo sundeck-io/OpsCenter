6829d8e23499111a38b7a5b7ec799901e896bd83
Susannah Marcus <66705808+susannahmarcus@users.noreply.github.com>
2023-09-14 14:27:05 -0700

    272: Display error text when error on screen during e2e tests
    closes #272

--- cypress/support/utils.ts
+++ cypress/support/utils.ts
@@ -8,7 +8,17 @@ export function checkNoErrorOnThePage() {
   checkForLoading();
 
   // Validate no exception is thrown
-  cy.get('div[class="stException"]').should("not.exist");
+  cy.get('div[class="stException"]')
+    // stops the test from throwing it's own error so we can retrieve the internal text
+    .should(() => {
+      return;
+    })
+    // allows us to grab the internal text of the error
+    .then(($errorElem) => {
+      if ($errorElem.length > 0) {
+        throw new Error(`Error on screen: ${$errorElem.text()}`);
+      }
+    });
 }
 
 // Check for Success notification with particular text presence
