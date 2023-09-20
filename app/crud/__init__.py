6af5d0d75f375554abbda5f3f8f6edce4b3b2d24
Josh Elser <josh@sundeck.io>
2023-09-20 14:36:18 -0400

    Restore dropped imports

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,10 +1,11 @@
 # Make the entrypoints for SQL procedures available
+from .common import create_entity, update_entity, delete_entity
 import snowflake.snowpark
 
 from .labels import PredefinedLabel
 from .session import snowpark_session
 
 
-def validate_predefined_labels(session: snowflake.snowpark.Session):
-    with snowpark_session(session) as txn:
+def validate_predefined_labels(sess: snowflake.snowpark.Session):
+    with snowpark_session(sess) as txn:
         PredefinedLabel.validate_all(txn)
