b0dc5f83e1fcec636d828b79b93d086aa3372355
Josh Elser <josh@sundeck.io>
2023-09-20 15:45:02 -0400

    Add an INTERNAL_PYTHON versioned schema
    We cannot create UDFs that reference code on the stage unless they are
    in a versioned schema. The only versioned schemas we have are
    user-facing, so make a new one.

--- test/unit/test_label_procs.py
+++ test/unit/test_label_procs.py
@@ -309,7 +309,7 @@ def test_create_grouped_then_ungrouped_label(conn, timestamp_string):
 
 
 def test_validate_predefined_label(conn, timestamp_string):
-    sql = "CALL INTERNAL.VALIDATE_PREDEFINED_LABELS();"
+    sql = "CALL INTERNAL_PYTHON.VALIDATE_PREDEFINED_LABELS();"
     assert run_proc(conn, sql) is None
 
 
