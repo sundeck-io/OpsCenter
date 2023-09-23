2304df363aab35fc3b76f38cfb6c875f3b7df310
Josh Elser <josh@sundeck.io>
2023-09-22 20:05:22 -0400

    Doesn't like empty files, either

--- bootstrap/011_warehouse_schedules.sql
+++
@@ -1,4 +0,0 @@
-
--- Create the WAREHOUSE_SCHEDULES table. Python procedures cannot be
--- called from a Native App setup script. Has to live in ADMIN.FINALIZE_SETUP()
--- call internal_python.create_table('WAREHOUSE_SCHEDULES');
