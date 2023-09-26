663b54e1ec11c13a4942a5e0d3338f7fe4acd70f
Ryan Murray <rymurr@gmail.com>
2023-09-26 10:30:29 +0200

    Don't include deleted users, they may be disputes
    closes #368

--- bootstrap/027_user_emails.sql
+++ bootstrap/027_user_emails.sql
@@ -5,7 +5,7 @@ CREATE OR REPLACE PROCEDURE internal.refresh_users() RETURNS STRING LANGUAGE SQL
 BEGIN
     BEGIN TRANSACTION;
         truncate table internal.sfusers;
-        insert into internal.sfusers select name, email from snowflake.account_usage.users;
+        insert into internal.sfusers select name, email from snowflake.account_usage.users where deleted_on is null;
         call internal.set_config('SNOWFLAKE_USER_MAINTENANCE', current_timestamp()::string);
     COMMIT;
 END;
