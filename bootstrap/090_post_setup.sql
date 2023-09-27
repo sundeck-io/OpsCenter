38a596b9dff1aaf8d4ffd26b62909e8618730aa6
Ryan Murray <rymurr@gmail.com>
2023-09-27 12:36:10 +0200

    Ensure outcome isn't forced to null (#372)
    closes #368

--- bootstrap/090_post_setup.sql
+++ bootstrap/090_post_setup.sql
@@ -149,7 +149,7 @@ BEGIN
                 WHEN other THEN
                     outcome2 := SQLERRM;
             END;
-            outcome := outcome || outcome2;
+            outcome := (select outcome || coalesce(outcome2, ''));
         end if;
 
         -- Send slack messages
@@ -171,7 +171,7 @@ Query Text: {bt}{bt}{bt}{query_text}{bt}{bt}{bt}
            WHEN other THEN
                slackResult := SQLERRM;
            END;
-           outcome := outcome || slackResult;
+            outcome := (select outcome || coalesce(slackResult, ''));
        END IF;
 
        let name string := act.PROBE_NAME;
