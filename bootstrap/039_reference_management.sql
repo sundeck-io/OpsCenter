cca520f373c6a0e34922bc8cfaebfe0e98a14ec4
Ryan Murray <rymurr@gmail.com>
2023-11-02 17:58:15 +0100

    Update bootstrap/039_reference_management.sql
    Co-authored-by: Josh Elser <josh.elser@gmail.com>

--- bootstrap/039_reference_management.sql
+++ bootstrap/039_reference_management.sql
@@ -15,7 +15,7 @@ begin
   case (operation)
     when 'ADD' then
        select system$set_reference(:ref_name, :ref_or_alias);
-        insert into internal.reference_management (ref_name, operation, ref_or_alias) values (:ref_name, 'Running external funcitons setup proc.', :ref_or_alias);
+        insert into internal.reference_management (ref_name, operation, ref_or_alias) values (:ref_name, 'Running external functions setup proc.', :ref_or_alias);
         call admin.setup_register_tenant_func();
     when 'REMOVE' then
        select system$remove_reference(:ref_name, :ref_or_alias);
