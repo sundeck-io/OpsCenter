24ee08b664443fd236fccf15d1d30846d46bad85
Josh Elser <josh@sundeck.io>
2023-11-15 16:04:38 -0500

    Add procedures for remaining "settings"  (#489)
    * Manages Snowflake tasks via Python, exposing simple ENABLE_TASK/DISABLE_TASK procedures
    * SQL Proc for instructions to enable diagnostics (I wanted to send back markdown but deploys fail with the triple backtick for codeblocks -- deferred until we want to do something in a UI with this output).
    * SQL wrappers around resetting QM&L's and QH/WEH data (does not address the fact that internal.merge_predefined_labels() did not work for me -- I have another issue already open for this)
    * Python crud unit tests for the tasks
    * Snowflake+Python tests for everything
    * * The qh/weh data reset is hidden behind a disabled-by-default marker because it is costly/slow.

---
+++ app/crud/tasks.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, validator
+from typing import ClassVar, Tuple
+
+
+class Task(BaseModel):
+    known_tasks: ClassVar[Tuple[str]] = (
+        "QUERY_HISTORY_MAINTENANCE",
+        "WAREHOUSE_EVENTS_MAINTENANCE",
+        "SFUSER_MAINTENANCE",
+        "USER_LIMITS_MAINTENANCE",
+    )
+
+    task_name: str
+
+    def enable(self, session):
+        session.sql(f"ALTER TASK TASKS.{self.task_name} RESUME").collect()
+
+    def disable(self, session):
+        session.sql(f"ALTER TASK TASKS.{self.task_name} SUSPEND").collect()
+
+    @validator("task_name", allow_reuse=True)
+    def validate_name(cls, value: str) -> str:
+        assert isinstance(value, str), "Task name must be a string"
+        # Normalize to uppercase and remove leading/trailing whitespace
+        value = value.strip().upper()
+        assert (
+            value in cls.known_tasks
+        ), f"Unknown task {value}, known tasks are {cls.known_tasks}"
+        return value
--- test/unit/test_settings.py
+++ test/unit/test_settings.py
@@ -1,12 +1,28 @@
+import datetime
 import pytest
 
 
+datetime_format = "%Y-%m-%d %H:%M:%S.%f %z"
 default_settings = {
     "default_timezone": "America/Los_Angeles",
     "storage_cost": "40.0",
     "compute_credit_cost": "2.0",
     "serverless_credit_cost": "3.0",
 }
+
+
+def enable_task(cursor, name: str):
+    row = cursor.execute(
+        "call admin.enable_task(%(name)s)", params={"name": name}
+    ).fetchone()
+    return row[0]
+
+
+def disable_task(cursor, name: str):
+    row = cursor.execute(
+        "call admin.disable_task(%(name)s)", params={"name": name}
+    ).fetchone()
+    return row[0]
 
 
 def update_setting(cursor, key: str, value: str) -> str:
@@ -86,3 +102,95 @@ def test_update_bad_values(conn, key, badvalue):
     with conn() as cnx, cnx.cursor() as cur:
         result = update_setting(cur, key, badvalue)
         assert result != "", f"Should have seen error updating {key} with {badvalue}"
+
+
+@pytest.mark.parametrize(
+    "name,success",
+    [
+        ("QUERY_HISTORY_MAINTENANCE", True),
+        ("query_history_maintenance ", True),
+        ("WAREHOUSE_EVENTS_MAINTENANCE", True),
+        ("SFUSER_MAINTENANCE", True),
+        ("foo", False),
+    ],
+)
+def test_task_management(conn, name: str, success: bool):
+    with conn() as cnx, cnx.cursor() as cur:
+        resp = enable_task(cur, name)
+        if success:
+            assert resp == "", "Enabling task should not return an error"
+        else:
+            assert resp != "", "Enabling task should return an error"
+
+        resp = disable_task(cur, name)
+        if success:
+            assert resp == "", "Disabling task should not return an error"
+        else:
+            assert resp != "", "Disabling task should return an error"
+
+
+def test_diagnostics_proc(conn):
+    with conn() as cnx, cnx.cursor() as cur:
+        row = cur.execute("call admin.ENABLE_DIAGNOSTIC_INSTRUCTIONS()").fetchone()
+        assert len(row) == 1
+
+        instructions = row[0]
+        assert instructions != "", "Diagnostics should return instructions"
+        assert "CREATE EVENT" in instructions
+        assert "SET SHARE_EVENTS_WITH_PROVIDER" in instructions
+
+
+@pytest.mark.slow
+def test_reload_query_history(conn):
+    with conn() as cnx, cnx.cursor() as cur:
+        row = cur.execute(
+            "call internal.get_config('QUERY_HISTORY_MAINTENANCE')"
+        ).fetchone()
+        orig_qh_refresh = (
+            datetime.datetime.strptime(row[0], datetime_format)
+            if row[0]
+            else datetime.datetime.now()
+        )
+
+        row = cur.execute(
+            "call internal.get_config('WAREHOUSE_EVENTS_MAINTENANCE')"
+        ).fetchone()
+        orig_wh_refresh = (
+            datetime.datetime.strptime(row[0], datetime_format)
+            if row[0]
+            else datetime.datetime.now()
+        )
+
+        # Refresh the data
+        row = cur.execute("call admin.reload_query_history()").fetchone()
+        assert len(row) == 1
+
+        assert row[0] == "", "Reload query history should not return an error"
+
+        # TODO more exhaustive checks that the underlying procedures to refresh QH and WE data actually worked.
+        row = cur.execute(
+            "call internal.get_config('QUERY_HISTORY_MAINTENANCE')"
+        ).fetchone()
+        assert row[0] is not None, "Expected QUERY_HISTORY_MAINTENANCE time to be set"
+        assert (
+            datetime.datetime.strptime(row[0], datetime_format) > orig_qh_refresh
+        ), "Expected QUERY_HISTORY_MAINTENANCE time to be updated"
+
+        row = cur.execute(
+            "call internal.get_config('WAREHOUSE_EVENTS_MAINTENANCE')"
+        ).fetchone()
+        assert (
+            row[0] is not None
+        ), "Expected WAREHOUSE_EVENTS_MAINTENANCE time to be set"
+        assert (
+            datetime.datetime.strptime(row[0], datetime_format) > orig_wh_refresh
+        ), "Expected WAREHOUSE_EVENTS_MAINTENANCE time to be updated"
+
+
+def test_reload_probes_and_labels(conn):
+    with conn() as cnx, cnx.cursor() as cur:
+        row = cur.execute("call admin.reload_preconfigured_data()").fetchone()
+        assert len(row) == 1
+
+        assert row[0] == "", "Reload preconfigured data should not return an error"
+        # TODO more exhaustive checks that labels and query monitors were re-created
