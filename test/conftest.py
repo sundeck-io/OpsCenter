76650504e492f8c3afff83acfc47c365f30b4079
Josh Elser <josh@sundeck.io>
2023-11-15 16:04:38 -0500

    Add procedures for remaining "settings"  (#489)
    * Manages Snowflake tasks via Python, exposing simple ENABLE_TASK/DISABLE_TASK procedures
    * SQL Proc for instructions to enable diagnostics (I wanted to send back markdown but deploys fail with the triple backtick for codeblocks -- deferred until we want to do something in a UI with this output).
    * SQL wrappers around resetting QM&L's and QH/WEH data (does not address the fact that internal.merge_predefined_labels() did not work for me -- I have another issue already open for this)
    * Python crud unit tests for the tasks
    * Snowflake+Python tests for everything
    * * The qh/weh data reset is hidden behind a disabled-by-default marker because it is costly/slow.

--- test/conftest.py
+++ test/conftest.py
@@ -20,6 +20,23 @@ def pytest_addoption(parser):
         default="opscenter",
         help="Connection profile name as specified in ~/.snowsql/config",
     )
+    parser.addoption(
+        "--runslow", action="store_true", default=False, help="run slow/expensive tests"
+    )
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "slow: mark test as slow or expensive to run")
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--runslow"):
+        # --runslow given in cli: do not skip slow tests
+        return
+    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+    for item in items:
+        if "slow" in item.keywords:
+            item.add_marker(skip_slow)
 
 
 # Returns value of --profile argument
