ee26939e2082d971bfbc7aaea8080d4be64c1e7e
Josh Elser <josh@sundeck.io>
2023-11-28 16:28:29 -0500

    Add new procedure to link OpsCenter to Sundeck (#498)
    * Use SYSTEM$GET_ALL_REFERENCES, add context-specific error messages, simplify scripting
    * Don't trash the reference_management table

--- bootstrap/039_reference_management.sql
+++ bootstrap/039_reference_management.sql
@@ -1,5 +1,5 @@
 
-create or replace table internal.reference_management (
+create table if not exists internal.reference_management (
     ref_name string,
     operation string,
     ref_or_alias string
@@ -11,16 +11,18 @@ create or replace procedure admin.update_reference(ref_name string, operation st
  returns string
  as $$
 begin
-  insert into internal.reference_management (ref_name, operation, ref_or_alias) values (:ref_name, :operation, :ref_or_alias);
+  SYSTEM$LOG_INFO('Updating reference: ' || ref_name || ' operation: ' || operation || ' ref_or_alias: ' || ref_or_alias);
   case (operation)
     when 'ADD' then
        select system$set_reference(:ref_name, :ref_or_alias);
-        insert into internal.reference_management (ref_name, operation, ref_or_alias) values (:ref_name, 'Running external functions setup proc.', :ref_or_alias);
-        call admin.setup_register_tenant_func();
+       insert into internal.reference_management (ref_name, operation, ref_or_alias) values (:ref_name, 'Running external functions setup proc.', :ref_or_alias);
+       call admin.setup_register_tenant_func();
     when 'REMOVE' then
        select system$remove_reference(:ref_name, :ref_or_alias);
+       delete from internal.reference_management where name = :ref_name;
     when 'CLEAR' then
        select system$remove_all_references(:ref_name);
+       delete from internal.reference_management where name = :ref_name;
     else
        return 'Unknown operation: ' || operation;
   end case;
