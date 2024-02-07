316a6b5cfb7bf0a413a2034bdf4ae3305e0b405f
Ryan Murray <rymurr@gmail.com>
2024-02-07 12:37:47 +0100

    Add utilisation udtf for saas

--- bootstrap/020_warehouse_utilzation.sql
+++ bootstrap/020_warehouse_utilzation.sql
@@ -70,3 +70,21 @@ FROM
     QUERY_WH_UTIL
     FULL OUTER JOIN WAREHOUSE_PERIODIC ON QUERY_WH_UTIL.WAREHOUSE_ID = WAREHOUSE_PERIODIC.WAREHOUSE_ID AND ST_PERIOD = M_PERIOD;
 ;
+
+create or replace function reporting.WAREHOUSE_UTILIZATION_HEATMAP(INPUT_START_DATE timestamp, INPUT_END_DATE timestamp, INPUT_WAREHOUSE_NAME string)
+returns table (period timestamp, utilization number(38,12))
+as $$
+	with ccost as (
+	  select coalesce(value, 2.0) as value from internal.config where key='compute_credit_cost'),
+    util as (
+        select date_trunc('day', period) as period, sum(loaded_cc * (select any_value(value) from ccost)) as cost, iff(sum(loaded_cc) = 0,null, sum(unloaded_cc)/sum(loaded_cc)) as utilization
+        from reporting.warehouse_daily_utilization
+        where period between INPUT_START_DATE and INPUT_END_DATE and case when INPUT_WAREHOUSE_NAME is null then true else warehouse_name = INPUT_WAREHOUSE_NAME end
+        group by 1
+	)
+
+    select cast(d.date as timestamp) as period, utilization from
+    internal.dates d
+    left outer join util u on d.date = u.period
+    where d.date between INPUT_START_DATE and INPUT_END_DATE
+$$;
