bf78d6bac940177e14d86d40570d80b16b825d8c
Ryan Murray <rymurr@gmail.com>
2024-02-09 14:37:12 +0100

    Don't call python to set settings

--- bootstrap/050_settings.sql
+++ bootstrap/050_settings.sql
@@ -12,25 +12,15 @@ END;
 
 CREATE OR REPLACE PROCEDURE ADMIN.UPDATE_SETTING(name TEXT, value TEXT)
     RETURNS TEXT
-    LANGUAGE PYTHON
-    RUNTIME_VERSION = "3.10"
-    HANDLER = 'run'
-    PACKAGES = ('snowflake-snowpark-python', 'pydantic')
-    IMPORTS = ('{{stage}}/python/crud.zip')
+    LANGUAGE SQL
     EXECUTE AS OWNER
 AS
 $$
-from crud.base import transaction
-from crud.errors import summarize_error
-from crud.settings import Setting
-def run(bare_session, name: str, value: str):
-    with transaction(bare_session) as session:
-        try:
-            setting = Setting(key=name, value=value)
-            setting.write(session)
-            return ""
-        except Exception as ve:
-            return summarize_error("Failed to update setting", ve)
+BEGIN
+    let res text := '';
+    call internal.set_config(:name, :value) into :res;
+    return res;
+END;
 $$;
 
 CREATE OR REPLACE PROCEDURE ADMIN.ENABLE_TASK(name TEXT)
