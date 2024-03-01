e0716a68d403a4d5c1fe14c5ad49ee600a82bb0f
Anshul Data <anshul@sundeck.io>
2024-03-01 18:32:09 +0530

    Fix Column Names

--- bootstrap/043_routines.sql
+++ bootstrap/043_routines.sql
@@ -1,15 +1,16 @@
 
 
 create or replace function admin.list_routines()
-    returns TABLE (name VARCHAR, namespace VARCHAR, refCount INT, schema VARCHAR, comment VARCHAR, childHooks VARCHAR)
+    returns TABLE (routine_name VARCHAR, routine_namespace VARCHAR, routine_created_at VARCHAR, routine_last_modified_at VARCHAR, routine_ref_count INT, routine_ddl VARCHAR, routine_comment VARCHAR)
 as
 $$
     SELECT
     value:name::string,
     value:namespace::string,
+    value:createdAt::string,
+    value:lastModifiedAt::string,
     value:refCount::int,
-    value:schema::string,
-    value:comment::string,
-    value:childHooks::string
+    value:ddl::string,
+    value:comment::string
     FROM TABLE(FLATTEN(input => internal.wrapper_list_routines(object_construct())))
 $$;
