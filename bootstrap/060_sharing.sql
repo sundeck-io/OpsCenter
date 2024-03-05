e6d7a90eafd4625234821d6d40ea00f489a62372
Josh Elser <josh@sundeck.io>
2024-03-05 12:46:45 -0500

    Drop columns used for filtering from view definition (#571)

--- bootstrap/060_sharing.sql
+++ bootstrap/060_sharing.sql
@@ -3,5 +3,5 @@
 -- Access to the SUNDECK database is given to application_package in deploy.py
 -- SHARING.GLOBAL_QUERY_HISTORY is already filtered on SNOWFLAKE_ACCOUNT_LOCATOR = CURRENT_ACCOUNT()
 CREATE OR REPLACE VIEW REPORTING.SUNDECK_QUERY_HISTORY
-   AS SELECT * from SHARING.GLOBAL_QUERY_HISTORY WHERE
+   AS SELECT * EXCLUDE(SUNDECK_ACCOUNT_ID) from SHARING.GLOBAL_QUERY_HISTORY WHERE
      UPPER(SUNDECK_ACCOUNT_ID) = (select UPPER(value) from internal.config where key = 'tenant_id');
