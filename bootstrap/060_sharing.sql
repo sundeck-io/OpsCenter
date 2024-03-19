e9a8dc1aafa32943090e97dd8479f83593235012
Josh Elser <josh@sundeck.io>
2024-03-19 11:45:05 -0400

    Enumerate columns for sundeck_query_history to not break when new col… (#587)
    * Enumerate columns for sundeck_query_history to not break when new columns are added
    
    * Drop the earlier-filtered columns from the app view
    
    * Update precommit fixtures for new schema
    
    lift another big block of SQL from devdeploy into its own file

--- bootstrap/060_sharing.sql
+++ bootstrap/060_sharing.sql
@@ -1,7 +1,23 @@
 
-
 -- Access to the SUNDECK database is given to application_package in deploy.py
 -- SHARING.GLOBAL_QUERY_HISTORY is already filtered on SNOWFLAKE_ACCOUNT_LOCATOR = CURRENT_ACCOUNT()
 CREATE OR REPLACE VIEW REPORTING.SUNDECK_QUERY_HISTORY
-   AS SELECT * EXCLUDE(SUNDECK_ACCOUNT_ID) from SHARING.GLOBAL_QUERY_HISTORY WHERE
-     UPPER(SUNDECK_ACCOUNT_ID) = (select UPPER(value) from internal.config where key = 'tenant_id');
+    AS SELECT
+        SNOWFLAKE_QUERY_ID,
+        SUNDECK_QUERY_ID,
+        FLOW_NAME,
+        QUERY_TEXT_RECEIVED,
+        QUERY_TEXT_FINAL,
+        ALT_WAREHOUSE_ROUTE,
+        SUNDECK_STATUS,
+        SUNDECK_ERROR_CODE,
+        SUNDECK_ERROR_MESSAGE,
+        USER_NAME,
+        ROLE_NAME,
+        SUNDECK_START_TIME,
+        SNOWFLAKE_SUBMISSION_TIME,
+        SNOWFLAKE_END_TIME,
+        ACTIONS_EXECUTED,
+        SCHEMA_ONLY_REQUEST
+    from SHARING.GLOBAL_QUERY_HISTORY WHERE
+        UPPER(SUNDECK_ACCOUNT_ID) = (select UPPER(value) from internal.config where key = 'tenant_id');
