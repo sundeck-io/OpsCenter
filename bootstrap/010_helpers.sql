afd1ad80a2b42f8c8b914446ac4a9400aa9787e7
Chandra Sanapala <chandra@sundeck.io>
2024-05-10 12:34:05 +0530

    Update 010_helpers.sql - Create procedure to create_warehouses_with_sizes

--- bootstrap/010_helpers.sql
+++ bootstrap/010_helpers.sql
@@ -149,3 +149,80 @@ $$
 
   return str;
 $$;
+
+create or replace procedure tools.warehouse_size_as_int(v VARCHAR)
+    returns int
+    language sql
+as
+declare
+    INVALID_WAREHOUSE_SIZE EXCEPTION(-20511, 'No such warehouse size, valid sizes are \'X-Small\', \'Small\', \'Medium\',  \'Large\',  \'X-Large\', \'2X-Large\', \'3X-Large\', \'4X-Large\', \'5X-Large\', \'6X-Large\' ');
+begin
+    case (v)
+      when 'X-Small'  then return 1;
+      when 'Small'    then return 2;
+      when 'Medium'   then return 3;
+      when 'Large'    then return 4;
+      when 'X-Large'  then return 5;
+      when '2X-Large' then return 6;
+      when '3X-Large' then return 7;
+      when '4X-Large' then return 8;
+      when '5X-Large' then return 9;
+      when '6X-Large' then return 10;
+      else raise INVALID_WAREHOUSE_SIZE;
+    end;
+end;
+
+create or replace procedure tools.create_warehouses_with_sizes(prefix string, min_size string, max_size string)
+    returns string
+    language sql
+as
+begin
+    let min int := 0;
+    let max int := 0;
+    call warehouse_size_as_int(:min_size) INTO min;
+    call warehouse_size_as_int(:max_size) INTO max;
+
+    let sql string := 'select column1 as size, column2 as credit
+            from values (\'X-Small\', 1), (\'Small\', 2), (\'Medium\', 3), (\'Large\', 4), (\'X-Large\', 5),
+            (\'2X-Large\', 6), (\'3X-Large\', 7), (\'4X-Large\', 8), (\'5X-Large\', 9), (\'6X-Large\', 10)
+            WHERE credit >= ? AND credit <= ?';
+    let rs resultset := (execute immediate sql using (min, max));
+
+    let wh_names string := '';
+    let c1 cursor for rs;
+    for record in c1 DO
+        let wh_name string := :prefix || '_' || record.size;
+        let cmd string := 'CREATE WAREHOUSE IF NOT EXISTS "' || wh_name || '" WITH WAREHOUSE_SIZE =\'' || record.size || '\' AUTO_SUSPEND = 60 INITIALLY_SUSPENDED = TRUE';
+        execute immediate :cmd;
+        wh_names := wh_names || ', "' || wh_name || '"';
+    end for;
+    return 'Successfully created warehouses' || wh_names;
+end;
+
+
+create or replace procedure tools.drop_warehouses_with_sizes(prefix string, min_size string, max_size string)
+    returns string
+    language sql
+as
+begin
+    let min int := 0;
+    let max int := 0;
+    call warehouse_size_as_int(:min_size) INTO min;
+    call warehouse_size_as_int(:max_size) INTO max;
+
+    let sql string := 'select column1 as size, column2 as credit
+            from values (\'X-Small\', 1), (\'Small\', 2), (\'Medium\', 3), (\'Large\', 4), (\'X-Large\', 5),
+            (\'2X-Large\', 6), (\'3X-Large\', 7), (\'4X-Large\', 8), (\'5X-Large\', 9), (\'6X-Large\', 10)
+            WHERE credit >= ? AND credit <= ?';
+    let rs resultset := (execute immediate sql using (min, max));
+
+    let wh_names string := '';
+    let c1 cursor for rs;
+    for record in c1 DO
+        let wh_name string := :prefix || '_' || record.size;
+        let cmd string := 'DROP WAREHOUSE IF EXISTS "' || wh_name || '"';
+        execute immediate :cmd;
+        wh_names := wh_names || ', "' || wh_name || '"';
+    end for;
+    return 'Successfully dropped warehouses' || wh_names;
+end;
